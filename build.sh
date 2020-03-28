705450cdf3adde090eabb40bad675f5b1658c446
Nico <nicochico.link@gmail.com>
2020-03-28 17:48:24 +0100

    .gitignore updated

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# mkdir bin obj
+mkdir -p bin/ obj/
 g++ -Wall -fexceptions -g -Iinclude -c "src/Lab1.cpp" -o "obj/Lab1.o"
 g++ -Wall -fexceptions -g -Iinclude -c "src/Population.cpp" -o "obj/Population.o"
 g++ -Wall -fexceptions -g -Iinclude -c "src/Task.cpp" -o "obj/Task.o"
